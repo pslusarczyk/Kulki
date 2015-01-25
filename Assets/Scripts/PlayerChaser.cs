a487107921f266ba1164347af1affc99f421704a
pslusarczyk <pawel.slusarczyk@interia.eu>
2015-01-25 01:55:27 +0100

    Saple scene with simple Player and PlayerChasers

---
+++ Assets/Scripts/PlayerChaser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerChaser : MonoBehaviour
+{
+   public float MaxVelocitySqr = 1;
+   public float Kick = 3;
+
+   // Use this for initialization
+   void Start()
+   {
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      var xToPlayer = 0 - transform.position.x;//GameObject.Find("Player").transform.position.x - transform.position.x;
+      var yToPlayer = 0 - transform.position.y;//GameObject.Find("Player").transform.position.y - transform.position.y;
+      var inputAndSpeedSignsAreSameForX = !(rigidbody2D.velocity.x * xToPlayer < 0);
+      var inputAndSpeedSignsAreSameForY = !(rigidbody2D.velocity.y * yToPlayer < 0);
+      var x = Mathf.Abs(rigidbody2D.velocity.x) > MaxVelocitySqr && inputAndSpeedSignsAreSameForX ? 0 : xToPlayer;
+      var y = Mathf.Abs(rigidbody2D.velocity.y) > MaxVelocitySqr && inputAndSpeedSignsAreSameForY ? 0 : yToPlayer;
+      var forceToAdd = new Vector2(x, y) * Kick;
+      rigidbody2D.AddForce(forceToAdd);
+   }
+}
--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Steering : MonoBehaviour
+{
+   public float MaxVelocitySqr = 2;
+   public float Kick = 8;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+	   var inputX = Input.GetAxis("Horizontal");
+      var inputY = Input.GetAxis("Vertical");
+	   var inputAndSpeedSignsAreSameForX = !(rigidbody2D.velocity.x * inputX < 0);
+	   var inputAndSpeedSignsAreSameForY = !(rigidbody2D.velocity.y * inputY < 0);
+      var x = Mathf.Abs(rigidbody2D.velocity.x) > MaxVelocitySqr && inputAndSpeedSignsAreSameForX ? 0 : inputX;
+      var y = Mathf.Abs(rigidbody2D.velocity.y) > MaxVelocitySqr && inputAndSpeedSignsAreSameForY ? 0 : inputY;
+	   var forceToAdd = new Vector2(x, y) * Kick * rigidbody2D.mass;
+      rigidbody2D.AddForce(forceToAdd);
 	}
 }
