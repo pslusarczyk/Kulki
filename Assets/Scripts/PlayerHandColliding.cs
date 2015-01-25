03ef07ec53024bb41999552653081b2ee7bcd48a
pslusarczyk <pawel.slusarczyk@interia.eu>
2015-01-25 18:21:49 +0100

    Dealed with the hand collision problem

---
+++ Assets/Scripts/PlayerHandColliding.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerHandColliding : MonoBehaviour {
+
+   public Rigidbody2D PlayerAnchor;
+
+   private Rigidbody2D _catchedBody;
+
+   void Update()
+   {
+      if (Input.GetKeyDown("space") && _catchedBody != null)
+      {
+         Destroy(PlayerAnchor.gameObject.GetComponents<DistanceJoint2D>()
+            .First(c => c.connectedBody == _catchedBody));
+         _catchedBody.GetComponent<PlayerChaser>().enabled = true;
+         _catchedBody = null;
+      }
+   }
+
+   void OnTriggerEnter2D(Collider2D metCollider)
+   {
+      if (_catchedBody == null && metCollider.gameObject.CompareTag("Catchable"))
+      {
+         _catchedBody = metCollider.rigidbody2D;
+         _catchedBody.GetComponent<PlayerChaser>().enabled = false;
+         var joint = PlayerAnchor.gameObject.AddComponent<DistanceJoint2D>();
+         joint.connectedBody = _catchedBody;
+         joint.distance = 1.2f;
+      }
+   }
+}
---
+++ Assets/Scripts/PlayerHandSteering.cs
@@ -0,0 +1,19 @@
+﻿using Tools;
+using UnityEngine;
+
+public class PlayerHandSteering : MonoBehaviour
+{
+   public PlayerHandColliding PlayerHandColliding;
+   
+	// Update is called once per frame
+	void Update () {
+      var inputX = Input.GetAxis("Horizontal2");
+      var inputY = Input.GetAxis("Vertical2");
+	   var handPosition = new Vector2(inputX, inputY);
+	   const float minimalHandMagnitude = .2f;
+      GetComponent<SpriteRenderer>().enabled = handPosition.sqrMagnitude > minimalHandMagnitude;
+      GetComponent<SpriteRenderer>().material.SetAlpha(handPosition.sqrMagnitude);
+	   transform.localPosition = handPosition * .5f;
+	   PlayerHandColliding.transform.position = transform.position;
+	}
+}
