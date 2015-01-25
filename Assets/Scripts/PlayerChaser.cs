d4822c4f79066bb1436a1c2d3e74ef1ce67a2372
pslusarczyk <pawel.slusarczyk@interia.eu>
2015-01-26 00:46:07 +0100

    Chasers are chasing player and are fixed

--- Assets/Scripts/PlayerChaser.cs
+++ Assets/Scripts/PlayerChaser.cs
@@ -2,8 +2,19 @@
 
 public class PlayerChaser : MonoBehaviour
 {
-   public float MaxVelocitySqr = 1;
-   public float Kick = 80;
+   private GameObject _playerObject;
+
+   public float MaxVelocitySqr;
+   public float Kick;
+
+   public GameObject PlayerObject()
+   {
+      if (_playerObject == null)
+      {
+         _playerObject = GameObject.Find("Player");
+      }
+      return _playerObject;
+   }
 
    // Use this for initialization
    void Start()
@@ -13,8 +24,8 @@ public class PlayerChaser : MonoBehaviour
    // Update is called once per frame
    void FixedUpdate()
    {
-      var xToPlayer = 0 - transform.position.x;//GameObject.Find("Player").transform.position.x - transform.position.x;
-      var yToPlayer = 0 - transform.position.y;//GameObject.Find("Player").transform.position.y - transform.position.y;
+      var xToPlayer = PlayerObject().transform.position.x - transform.position.x;
+      var yToPlayer = PlayerObject().transform.position.y - transform.position.y;
       var inputAndSpeedSignsAreSameForX = !(rigidbody2D.velocity.x * xToPlayer < 0);
       var inputAndSpeedSignsAreSameForY = !(rigidbody2D.velocity.y * yToPlayer < 0);
       var x = Mathf.Abs(rigidbody2D.velocity.x) > MaxVelocitySqr && inputAndSpeedSignsAreSameForX ? 0 : xToPlayer;
