2a4a967bddd708445bb25d3449a021af11b993fd
pslusarczyk <pawel.slusarczyk@interia.eu>
2015-01-27 01:24:08 +0100

    Moved prefabs dir, added Breeder and Pit

---
+++ Assets/Scripts/Breeder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breeder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	InvokeRepeating("Breed", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+
+   void Breed()
+   {
+      Instantiate(Resources.Load<PlayerChaser>("Prefabs/PlayerChaser"), transform.position, transform.rotation);
+      Debug.Log(Time.deltaTime);
+   }
+}
--- Assets/Scripts/PlayerChaser.cs
+++ Assets/Scripts/PlayerChaser.cs
@@ -30,7 +30,7 @@ public class PlayerChaser : MonoBehaviour
       var inputAndSpeedSignsAreSameForY = !(rigidbody2D.velocity.y * yToPlayer < 0);
       var x = Mathf.Abs(rigidbody2D.velocity.x) > MaxVelocitySqr && inputAndSpeedSignsAreSameForX ? 0 : xToPlayer;
       var y = Mathf.Abs(rigidbody2D.velocity.y) > MaxVelocitySqr && inputAndSpeedSignsAreSameForY ? 0 : yToPlayer;
-      var forceToAdd = new Vector2(x, y) * Kick * Time.fixedDeltaTime;
+      var forceToAdd = new Vector2(x, y) * Kick * Time.fixedDeltaTime * rigidbody2D.mass;
       rigidbody2D.AddForce(forceToAdd);
    }
 }
