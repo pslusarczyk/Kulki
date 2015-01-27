b73accd9f578f1c01837419191db03ffdd25e434
pslusarczyk <pawel.slusarczyk@interia.eu>
2015-01-27 11:37:15 +0100

    Improved jumping mechanism

---
+++ Assets/Scripts/JumpBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpBar : MonoBehaviour
+{
+   public Color LoadingColor;
+   public Color FullColor;
+
+   public float Value { get; set; }
+
+   // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	   GetComponent<SpriteRenderer>().enabled = Value > 0;
+      GetComponent<SpriteRenderer>().color = Value > .99 ? FullColor : LoadingColor;
+
+      var inputX = Input.GetAxis("Horizontal");
+      var inputY = Input.GetAxis("Vertical");
+
+      var scale = transform.localScale;
+      transform.localScale = new Vector3(scale.x, Value * .25f - .01f, 1);
+
+      if (Mathf.Abs(inputX) + Mathf.Abs(inputY) < .01 && Value < 1)
+         Value = Value + Mathf.Clamp01(Time.deltaTime);
+	}
+}
--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -4,27 +4,38 @@ public class Steering : MonoBehaviour
 {
    public float MaxVelocitySqr = 2;
    public float Kick = 240000;
-   
+   public GameObject JumpBarObject;
+   private const float JumpForceMultiplier = 60;
+
 
 	// Use this for initialization
 	void Start ()
 	{
+      
 	}
+
+   void Update()
+   {
+
+
+   }
 	
-	// Update is called once per frame
 	void FixedUpdate ()
 	{
 	   var inputX = Input.GetAxis("Horizontal");
       var inputY = Input.GetAxis("Vertical");
 	   var inputAndSpeedSignsAreSameForX = !(rigidbody2D.velocity.x * inputX < 0);
 	   var inputAndSpeedSignsAreSameForY = !(rigidbody2D.velocity.y * inputY < 0);
-      var x = Mathf.Abs(rigidbody2D.velocity.x) > MaxVelocitySqr && inputAndSpeedSignsAreSameForX ? 0 : inputX;
-      var y = Mathf.Abs(rigidbody2D.velocity.y) > MaxVelocitySqr && inputAndSpeedSignsAreSameForY ? 0 : inputY;
-	   var forceToAdd = new Vector2(x, y) * Kick * Time.fixedDeltaTime * rigidbody2D.mass;
-	   if (Input.GetKeyDown(KeyCode.LeftShift))
+      var forceX = Mathf.Abs(rigidbody2D.velocity.x) > MaxVelocitySqr && inputAndSpeedSignsAreSameForX ? 0 : inputX;
+      var forceY = Mathf.Abs(rigidbody2D.velocity.y) > MaxVelocitySqr && inputAndSpeedSignsAreSameForY ? 0 : inputY;
+	   
+      var forceFactor = Kick*Time.fixedDeltaTime*rigidbody2D.mass;
+      var forceToAdd = new Vector2(forceX, forceY) * forceFactor;
+	   if (Input.GetKey(KeyCode.K))
 	   {
-	      forceToAdd *= 50;
-	      transform.localScale *= -1;
+         forceToAdd += forceToAdd.normalized * forceFactor 
+                              * JumpBarObject.GetComponent<JumpBar>().Value * JumpForceMultiplier;
+         JumpBarObject.GetComponent<JumpBar>().Value = 0;
 	   }
       rigidbody2D.AddForce(forceToAdd);
 	}
